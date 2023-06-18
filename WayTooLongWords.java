3dd2d33e551516518fdac44a9b5c264aaf0604df
mona0101 <121684356+mona0101@users.noreply.github.com>
2023-06-18 23:59:28 +0300

    Add files via upload

--- WayTooLongWords.java
+++ WayTooLongWords.java
@@ -1,41 +1,38 @@
-
 package waytoolongwords;
-
 import java.util.Scanner;
-import java.util.ArrayList;
+
 public class WayTooLongWords {
+
     public static void main(String[] args) {
-        
+
         Scanner scan = new Scanner(System.in);
+        int number;
+        int count =0;
+        number = scan.nextInt();
 
-        int num;
-        num = scan.nextInt();
-        scan.nextLine();
-         ArrayList <String> str = new ArrayList<String> ();
-        for (int i = 0; i < num; i++) {
-            String v = scan.nextLine().toLowerCase();
-            str.add(v);
-            
+        int values[][] = new int[number][3];
+
+        for (int i = 0; i < number; i++) {
+            for (int j = 0; j < 3; j++) {
+                values[i][j] = scan.nextInt();
+            }
         }
-        for(int i =0; i< num ; i++){
-            if (str.get(i).length() <= 10) {
-                System.out.println(str.get(i));
-            }
-            
-            else if (str.get(i).length() > 10) {
-                char firstChar, lastChar;int nuberOfChar =0;
-                firstChar = str.get(i).charAt(0);
-                lastChar = str.get(i).charAt(str.get(i).length()-1);
-                for (int j = 0; j < str.get(i).length()-1; j++) {
-                     nuberOfChar =+j;
-                }
-
-                System.out.printf("%c%d%c\n", firstChar,nuberOfChar, lastChar);
-
-            }
-
-        } 
         
+        
+       
+
+        for (int i = 0; i < number; i++) {
+            int sum = 0;
+            for (int j = 0; j < 3; j++) {
+                sum += values[i][j];  
+            }
+        if (sum == 2 || sum == 3) {
+                  count++;
+            }  
+        }
+
+        System.out.println(count);
+
     }
-    
+
 }
